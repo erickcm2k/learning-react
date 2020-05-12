dfe0be25fd5f857be1777c7f034230130ecb0f80
Erick Castañeda Martínez <erickce40@gmail.com>
2020-05-12 16:19:57 -0500

    babel - optimizando para cualquier navegador

--- platzi/clase-1/holamundo/src/index.js
+++ platzi/clase-1/holamundo/src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import HolaMundo from './components/HolaMundo'
 import * as serviceWorker from './serviceWorker';
+import HolaMundo from './components/HolaMundo'
+import Button from './components/Button'
 
 ReactDOM.render(
   <React.StrictMode>
-    <HolaMundo />
+    <Button/>
   </React.StrictMode>,
   document.getElementById('root')
-);
+);  
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ platzi/platzi-video/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom' // Para 'empujar' el código al navegador
+import HelloWorld from './components/HelloWorld'
+ReactDOM.render(<HelloWorld/>, document.getElementById(app)) // Componente, ruta a 'empujar'
+
