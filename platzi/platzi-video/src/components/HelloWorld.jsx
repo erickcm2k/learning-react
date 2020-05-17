70f589dc27ccc49a3b9123c91c41952770dd04f4
Erick Castañeda Martínez <erickce40@gmail.com>
2020-05-16 21:39:18 -0500

    agregando webpack y webpack-dev-server

--- platzi/platzi-video/src/components/HelloWorld.jsx
+++ platzi/platzi-video/src/components/HelloWorld.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const HelloWorld = () => {
+const HelloWorld = () => (
     <h1>Hello World</h1>
-}
+)
 
 export default HelloWorld
 
-// npm install @babel/core babel-loader @babel/preset-env @babel/preset-react --save -dev
+
--- platzi/platzi-video/src/index.js
+++ platzi/platzi-video/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom' // Para 'empujar' el código al navegador
 import HelloWorld from './components/HelloWorld'
-ReactDOM.render(<HelloWorld/>, document.getElementById(app)) // Componente, ruta a 'empujar'
+
+
+
+ReactDOM.render(<HelloWorld/>, document.getElementById('app')) // Componente, ruta a 'empujar'
 
