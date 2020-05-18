86563ab261750e83ae45055461efb4fe27e199dd
Erick Castañeda Martínez <erickce40@gmail.com>
2020-05-18 13:28:04 -0500

    Establecida conexion con fake API

--- platzi/platzi-video/src/Containers/App.jsx
+++ platzi/platzi-video/src/Containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
 import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
@@ -7,37 +7,49 @@ import CarouselItem from '../components/CarouselItem'
 import Footer from '../components/Footer'
 import '../assets/styles/App.scss'
 
-const App = () => (
-    <div className="App">
-        <Header/>
-        <Search/>
+const App = () => {
+    const [ videos, setVideos ] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/initialState')
+            .then(response => response.json())
+            .then(data => setVideos(data))
+    }, [])
+
+    console.log(videos)
     
-         <Categories title="Mi lista">
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories> 
+        return(
+            <div className="App">
+                <Header/>
+                <Search/>
+            
+                <Categories title="Mi lista">
+                    <Carousel>
+                        <CarouselItem/>
+                        <CarouselItem/>
+                        <CarouselItem/>
+                        <CarouselItem/>
+                    </Carousel>
+                </Categories> 
 
-         <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories> 
+                <Categories title="Tendencias">
+                    <Carousel>
+                        <CarouselItem/>
+                        <CarouselItem/>
+                    </Carousel>
+                </Categories> 
 
-         <Categories title="Originales">
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories> 
+                <Categories title="Originales">
+                    <Carousel>
+                        <CarouselItem/>
+                        <CarouselItem/>
+                    </Carousel>
+                </Categories> 
 
-        <Footer/>
-    </div>
-)
+                <Footer/>
+            </div>
+        )
+    }
 
 export default App
 
