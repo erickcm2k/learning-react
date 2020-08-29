2655acd569652078769056553edcee87d440b51b
Erick Castañeda Martínez <erickce40@gmail.com>
2020-08-29 11:55:39 -0500

    delete platzi files

--- platzi/platzi-video/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom' // Para 'empujar' el código al navegador
-import App from './Containers/App'
-
-ReactDOM.render(<App/>, document.getElementById('app')) // Componente, ruta a 'empujar'
-
