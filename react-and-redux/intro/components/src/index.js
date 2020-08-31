8eb144abbfe86e418a1562af76ac4ed20beafa34
Erick Castañeda Martínez <erickce40@gmail.com>
2020-08-31 16:50:50 -0500

    class based compponents

--- react-and-redux/intro/components/src/index.js
+++ react-and-redux/intro/components/src/index.js
@@ -1,29 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail";
 import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        date="Yesterdar 3:00 PM"
-        comment="Cool info!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        date="Today 6:00 PM"
-        comment="Thanks for the support"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        date="Today 5:00 AM"
-        comment="Nice post"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <h4>Warning!</h4>
+        Are you sure you want to continue?</ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          date="Yesterdar 3:00 PM"
+          comment="Cool info!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          date="Today 6:00 PM"
+          comment="Thanks for the support"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          date="Today 5:00 AM"
+          comment="Nice post"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
---
+++ react-and-redux/intro/seasons/src/SeasonDisplay.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SeasonDisplay = (props) => {
+    return(
+        <div>Hello world</div>
+    );
+} 
+
+export default SeasonDisplay
