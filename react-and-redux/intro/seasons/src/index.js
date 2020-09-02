59a05c637fa74a35c43df72bb419328711363b70
Erick Castañeda Martínez <erickce40@gmail.com>
2020-09-01 21:27:00 -0500

    using state

--- react-and-redux/intro/seasons/src/index.js
+++ react-and-redux/intro/seasons/src/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import SeasonDisplay from "./SeasonDisplay";
 class App extends React.Component {
-  render() {
+  // Constructor should not initialize anything
+  // Fetchs and other methods goes at componentDidMount function.
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  // Equivalent to write a constructor
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (error) => console.log(error)
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (error) => this.setState({ errorMessage: error.message })
     );
-    return <div></div>;
+  }
+
+  render() {
+    return <div>Location: {this.state.lat}</div>;
   }
 }
 
