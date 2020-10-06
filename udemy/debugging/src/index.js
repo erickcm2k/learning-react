eb819a2168518d0cf160d7ee9e28207f4b43d1f3
Erick Castañeda Martínez <erickce40@gmail.com>
2020-10-05 21:53:54 -0500

    updating react and react dom - bug fixed

--- udemy/debugging/src/index.js
+++ udemy/debugging/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Containers/App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App appTitle="Person Manager" />, document.getElementById('root'));
-
+registerServiceWorker();
