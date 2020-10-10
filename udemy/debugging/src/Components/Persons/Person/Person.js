4d11f7c2d2c7c136b6132518accd5a854d225ed2
Erick Castañeda Martínez <erickce40@gmail.com>
2020-10-09 22:54:42 -0500

    wrapping adjacent jsx elements

--- udemy/debugging/src/Components/Persons/Person/Person.js
+++ udemy/debugging/src/Components/Persons/Person/Person.js
@@ -1,22 +1,23 @@
 import React from "react";
-
+import Auxiliary from "../../../HOC/Auxiliary";
 import classes from "./Person.css";
 
 class Person extends React.Component {
   render() {
     console.log("[Person] rendering");
     return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>
+      <Auxiliary>
+        <p key="k1" onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
-        <p>{this.props.children}</p>
+        <p key="k2">{this.props.children}</p>
         <input
+          key="k3"
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </div>
+      </Auxiliary>
     );
   }
 }
---
+++ udemy/debugging/src/HOC/Auxiliary.js
@@ -0,0 +1,7 @@
+import React from "react";
+// An auxiliary component is commonly used for wrapping
+// adjacente elements, satisfying React's requirement
+// of elements being wrapped in an enclosing tag.
+const Auxiliary = (props) => props.children;
+
+export default Auxiliary;
