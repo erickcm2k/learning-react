6449ebb58da78f45a7d93fe336399aaa212f31d3
Erick Castañeda Martínez <erickce40@gmail.com>
2020-12-29 13:18:16 -0600

    custom component styling

--- epic-react-kent-c-dodds/01-React-Fundamentals/src/exercise/05.js
+++ epic-react-kent-c-dodds/01-React-Fundamentals/src/exercise/05.js
@@ -12,9 +12,46 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+
+// const Box = props => {
+//   return (
+//     // Equivalent code.
+//     // <div
+//     //   style={props.style}
+//     //   className={props.className}
+//     //   children={props.children}
+//     // >{props.children}</div>
+//     <div {...props} />
+//   )
+// }
+const Box = props => {
+  const {size, style, ...otherProps} = props
+
+  const sizeClassName = size ? `box--${size}` : ''
+
+  return (
+    <div
+      className={`box ${sizeClassName}`.trim()}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
+const smallBox = (
+  <Box size="small" style={{backgroundColor: 'lightblue'}}>
+    small lightblue box
+  </Box>
+)
+const mediumBox = (
+  <Box size="medium" style={{backgroundColor: 'pink'}}>
+    medium pink box
+  </Box>
+)
+const largeBox = (
+  <Box size="large" style={{backgroundColor: 'orange'}}>
+    large orange box
+  </Box>
+)
 
 function App() {
   return (
