670699ed4a4e2b0fb82887761b5ca3e9392b4722
Erick Castañeda Martínez <erickce40@gmail.com>
2021-02-18 23:11:32 -0600

    Learning context

--- epic-react-kent-c-dodds/03-Advanced-React-Hooks/src/exercise/03.js
+++ epic-react-kent-c-dodds/03-Advanced-React-Hooks/src/exercise/03.js
@@ -3,36 +3,42 @@
 
 import * as React from 'react'
 
-// 🐨 create your CountContext here with React.createContext
+const CountContext = React.createContext()
 
-// 🐨 create a CountProvider component here that does this:
-//   🐨 get the count state and setCount updater with React.useState
-//   🐨 create a `value` array with count and setCount
-//   🐨 return your context provider with the value assigned to that array and forward all the other props
-//   💰 more specifically, we need the children prop forwarded to the context provider
+function useCountContext() {
+  const context = React.useContext(CountContext)
+  if (!context) {
+    throw new Error('useCount must me used within the CountProvider.')
+  }
+  return context
+}
+
+function CountProvider(props) {
+  const [count, setCount] = React.useState(0)
+  const value = [count, setCount]
+  return <CountContext.Provider value={value} {...props} />
+}
 
 function CountDisplay() {
-  // 🐨 get the count from useContext with the CountContext
-  const count = 0
+  const [count] = useCountContext()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  // 🐨 get the setCount from useContext with the CountContext
-  const setCount = () => {}
-  const increment = () => setCount(c => c + 1)
+  const [, setCount] = useCountContext()
+  const increment = () => {
+    setCount(c => c + 1)
+  }
   return <button onClick={increment}>Increment count</button>
 }
 
 function App() {
   return (
     <div>
-      {/*
-        🐨 wrap these two components in the CountProvider so they can access
-        the CountContext value
-      */}
-      <CountDisplay />
-      <Counter />
+      <CountProvider>
+        <CountDisplay />
+        <Counter />
+      </CountProvider>
     </div>
   )
 }
